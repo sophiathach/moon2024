68bd1ec5543fa603fba28effaa98f11f1af032f5
developer <developer@pop-os.localdomain>
2019-10-20 15:24:06 -0400

    Updates

---
+++ precache-manifest.f41b2f4fe5a5cfe09762ba70f3851d27.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f569f72c9d4cefa4ba9bb4f214bd43",
+    "url": "/moon2024/index.html"
+  },
+  {
+    "revision": "b613d821daea2bd46ebb",
+    "url": "/moon2024/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "94a0fbe1fb00dea864a6",
+    "url": "/moon2024/static/css/main.3c39c028.chunk.css"
+  },
+  {
+    "revision": "b613d821daea2bd46ebb",
+    "url": "/moon2024/static/js/2.8547f6d4.chunk.js"
+  },
+  {
+    "revision": "94a0fbe1fb00dea864a6",
+    "url": "/moon2024/static/js/main.fcbbafb4.chunk.js"
+  },
+  {
+    "revision": "16da7cfa81b4bf471202",
+    "url": "/moon2024/static/js/runtime-main.7fda05b4.js"
+  },
+  {
+    "revision": "e0688e2e8b089d91cb1860e1a0ca4b98",
+    "url": "/moon2024/static/media/Houston.e0688e2e.mp3"
+  },
+  {
+    "revision": "d88d08e03586565b4735535ba7a7c21a",
+    "url": "/moon2024/static/media/Ready.d88d08e0.mp3"
+  },
+  {
+    "revision": "aea72b77fe8dfe3b2fc19cb2d00b5cf3",
+    "url": "/moon2024/static/media/colony1.aea72b77.png"
+  },
+  {
+    "revision": "be1b5ccdee248ae69ac0d63a4206e63d",
+    "url": "/moon2024/static/media/colony2.be1b5ccd.png"
+  },
+  {
+    "revision": "6e81c9ff55e12a9c689ea83ce3d9ec9d",
+    "url": "/moon2024/static/media/door.6e81c9ff.svg"
+  },
+  {
+    "revision": "d77175d3320c07e4a74468c34d9de720",
+    "url": "/moon2024/static/media/earthfrommoon.d77175d3.jpeg"
+  },
+  {
+    "revision": "147514a3d0d325be38418baad6932df4",
+    "url": "/moon2024/static/media/southpole1.147514a3.jpeg"
+  },
+  {
+    "revision": "01a690fa6cadaa9c9b3fe88c46974ea2",
+    "url": "/moon2024/static/media/southpole2.01a690fa.jpeg"
+  },
+  {
+    "revision": "f317fc07a34d30c76763074921747b8b",
+    "url": "/moon2024/static/media/spaceman1.f317fc07.png"
+  },
+  {
+    "revision": "650d29daac6d99ec45587860b5f9ee67",
+    "url": "/moon2024/static/media/spaceman2.650d29da.png"
+  },
+  {
+    "revision": "40d888e3a9856aa7b759efc35a437d40",
+    "url": "/moon2024/static/media/tranquility.40d888e3.jpeg"
+  },
+  {
+    "revision": "c6a4c57998ac702e6cdab1a9e339b8a6",
+    "url": "/moon2024/static/media/tranquility2.c6a4c579.jpeg"
+  },
+  {
+    "revision": "22c40db6dd89b7706f821770c84a88c5",
+    "url": "/moon2024/static/media/white_noise.22c40db6.mp3"
+  },
+  {
+    "revision": "dabbd66ad3870f7ad38e47e40581ab74",
+    "url": "/moon2024/static/media/whole_story.dabbd66a.mp3"
+  }
+]);
